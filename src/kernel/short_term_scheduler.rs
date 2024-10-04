4e5aa71e68e8696cc1f2a37d295aff96b265ae2d
William Walker <turner.walker79@gmail.com>
2024-10-04 14:08:50 -0400

    Update short_term_scheduler.rs

--- src/kernel/short_term_scheduler.rs
+++ src/kernel/short_term_scheduler.rs
@@ -1,7 +1,43 @@
 /// Loads processes from RAM to the CPU.
 /// sets the CPU environment to the state described in the PCB.
-pub struct ShortTermSheduler {}
+use std::collections::{BinaryHeap, VecDeque};
 
-impl ShortTermSheduler {
-    pub fn start() {}
+use super::{Memory, ProcessControlBlock};
+
+// determines current algorithm
+pub enum SchedulingAlgorithm {
+    Priority,
+    FIFO,
+}
+
+pub struct ShortTermScheduler {
+    fifo_queue: VecDeque<ProcessControlBlock>,
+    priority_queue: BinaryHeap<ProcessControlBlock>,
+    scheduling_algo: SchedulingAlgorithm,
+}
+
+impl ShortTermScheduler {
+    pub fn new(scheduling_algo: SchedulingAlgorithm) -> ShortTermScheduler {
+        ShortTermScheduler {
+            priority_queue: BinaryHeap::new(),
+            fifo_queue: VecDeque::new(),
+            scheduling_algo,
+        }
+    }
+
+    // adds process to the correct queue based on algorithm
+    pub fn add_process(&mut self, pcb: ProcessControlBlock) {
+        match self.scheduling_algo {
+            SchedulingAlgorithm::Priority => self.priority_queue.push(pcb),
+            SchedulingAlgorithm::FIFO => self.fifo_queue.push_back(pcb),
+        }
+    }
+
+    // schedule process based on algorithm
+    pub fn schedule_next(&mut self, memory: &Memory) -> Option<ProcessControlBlock> {
+        match self.scheduling_algo {
+            SchedulingAlgorithm::Priority => self.priority_queue.pop(),
+            SchedulingAlgorithm::FIFO => self.fifo_queue.pop_front(),
+        }
+    }   
 }
