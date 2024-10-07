1ac85a83d21892b4a1a4414f180276ac73a3d49b
pk <pkartha@students.kennesaw.edu>
2024-10-07 13:48:22 -0400

    short term scheduler

--- src/kernel/short_term_scheduler.rs
+++ src/kernel/short_term_scheduler.rs
@@ -1,7 +1,30 @@
 /// Loads processes from RAM to the CPU.
 /// sets the CPU environment to the state described in the PCB.
+
+use crate::kernel::process_control_block
+
 pub struct ShortTermSheduler {}
 
 impl ShortTermSheduler {
-    pub fn start() {}
+    pub fn start() {}  
+
+    //create ready queue
+    pub struct ready_queue<{
+    ready_queue: Vec<u32>
+
+    }
+
+    //set CPU state to PCB state
+    pub fn setstate(){
+        
+    }
+
+    //set environment to PCB state
+    pub fn loadtoCPU(){
+        while(ready_queue.len != 0){
+            //push to CPU
+
+
+        }
+    }
 }
