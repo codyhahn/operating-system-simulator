f57a1cece090758a53c46f158f3245bd691db87a
pk <pkartha@students.kennesaw.edu>
2024-10-15 18:54:52 -0400

    Update core_dump.rs

--- src/io/core_dump.rs
+++ src/io/core_dump.rs
@@ -3,7 +3,16 @@ use std::io::prelude::*;
 
 fn coredump(memory : Vecdeque<u32>) -> std::io::Result<()> {
     let mut file = File::create("core dump");
-    while memory != 0 {
+
+    //write the jobs
+    file.write("JOB")
+    while memory != 0 & memory.len() > 15 {
     file.write(memory.pop_front());
+    }
+    
+    //write the data
+    file.write("DATA")
+    while memory != 0 {
+        file.write(memory.pop_front());
     }
 }
